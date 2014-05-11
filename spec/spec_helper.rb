cbfdc41688d7e1a6d8c7de8342086d4e82d16267
Dax <d.dax@email.com>
2014-05-11 12:09:55 +0200

    Run tests with Rake

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,6 @@ $LOAD_PATH.unshift('lib', 'spec')
 # require all support files
 Dir.glob('./spec/support/*.rb')  { |f| require f }
 
-# require all spec files
-Dir.glob('./spec/**/*spec.rb')  { |f| require f }
-
 require 'quotes'
 
 class Minitest::Spec
