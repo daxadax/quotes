57918be7c94b7e1ae9e0812c1c1824268786365d
Dax <d.dax@email.com>
2014-05-17 23:49:50 +0200

    Update factory_helpers

--- spec/support/factory_helpers.rb
+++ spec/support/factory_helpers.rb
@@ -1,12 +1,27 @@
 module Support
   module FactoryHelpers
 
-    def build_quote(options = {} )
+    def build_quote(options = {})
       author  = options[:author]  || 'Author'
       title   = options[:title]   || 'Title'
       content = options[:content] || 'Content'
 
       Entities::Quote.new(author, title, content, options)
+    end
+
+    def build_serialized_quote(options = {})
+      author  = options[:author]  || 'Author'
+      title   = options[:title]   || 'Title'
+      content = options[:content] || 'Content'
+
+      {
+        :author   => author,
+        :title    => title,
+        :content  => content,
+        :source   => options[:source] || nil,
+        :tags     => options[:tags]   || [],
+        :id       => options[:id]     || nil
+      }
     end
 
   end
