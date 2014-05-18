d9a181dece88d2d7910e39c078b308bd5316d836
Dax <d.dax@email.com>
2014-05-18 16:56:01 +0200

    Add ENV variables to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ Dir.glob('./spec/support/*.rb')  { |f| require f }
 
 require 'quotes'
 
+ENV['test'] = '1'
+ENV['DATABASE_URL'] = 'sqlite://quotes-test.db'
+
 class Minitest::Spec
   include Support::AssertionHelpers
   include Support::FactoryHelpers
