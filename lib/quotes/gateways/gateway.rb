cbd11f1a1a9b9047c96c26c9c0aaaa648d0a6515
Dax <d.dax@email.com>
2014-05-18 18:54:43 +0200

    Add JSON to gateway parent class

--- lib/quotes/gateways/gateway.rb
+++ lib/quotes/gateways/gateway.rb
@@ -1,16 +1,11 @@
+require 'json'
+
 module Gateways
   class Gateway
     include Support::ValidationHelpers
 
     def backend_for_quotes
-      raise "NO BACKEND YET!"
-    end
-
-    private
-
-    def ensure_valid!(quote)
-      ensure_kind_of!(quote)
-      ensure_not_persisted!(quote)
+      Gateways::QuotesGatewayBackend.new
     end
 
   end
