f215f8670f397674cbdec523e905e7494dbff3a5
Dax <d.dax@email.com>
2014-05-18 18:54:31 +0200

    Separate backend from gateway parent class

--- lib/quotes/gateways/backend.rb
+++ lib/quotes/gateways/backend.rb
@@ -2,9 +2,20 @@ require 'sqlite3'
 require 'sequel'
 
 module Gateways
-  class Backend < Gateway
+  class Backend
 
-    DB = Sequel.sqlite
+    def initialize
+      @database = retrieve_database
+    end
 
+    private
+
+    def retrieve_database
+      if ENV['test']
+        database = Sequel.connect('sqlite://quotes-test.db')
+      else
+        database = Sequel.connect('sqlite://quotes-development.db')
+      end
+    end
   end
 end
