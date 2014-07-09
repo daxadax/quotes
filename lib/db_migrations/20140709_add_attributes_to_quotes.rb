5f4097bbc3f63bdaa044c88e7c32463bf7ede06d
Dax <d.dax@email.com>
2014-07-09 09:56:03 +0200

    Add starred and links columns to quotes db

---
+++ lib/db_migrations/20140709_add_attributes_to_quotes.rb
@@ -0,0 +1,12 @@
+Sequel.migration do
+  up do
+    add_column :quotes, :starred, TrueClass
+    add_column :quotes, :links,   String, :text => true
+
+  end
+
+  down do
+    drop_column :quotes, :starred
+    drop_column :quotes, :links
+  end
+end
