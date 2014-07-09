be8dd7b9d525956d8d9ec5f957a04ca2999cabec
Dax <d.dax@email.com>
2014-07-09 18:13:37 +0200

    Update get_quote_spec

--- spec/use_cases/get_quote_spec.rb
+++ spec/use_cases/get_quote_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 class GetQuoteSpec < UseCaseSpec
 
-  let(:quote_id)    { create_quote }
-  let(:input)       { {:id => quote_id} }
-  let(:use_case)    { UseCases::GetQuote.new(input) }
+  let(:quote)     { create_quote }
+  let(:input)     { {:id => quote.id} }
+  let(:use_case)  { UseCases::GetQuote.new(input) }
 
   describe "call" do
     let(:result) { use_case.call }
 
     describe "with unexpected input" do
-      let(:quote_id) { '23' }
+      let(:quote) { build_quote }
 
       it "fails" do
         assert_failure { result }
@@ -18,10 +18,10 @@ class GetQuoteSpec < UseCaseSpec
     end
 
     it "retrieves the quote with the given quote_id" do
-      assert_equal quote_id,  result.id
-      assert_equal 'Author',  result.author
-      assert_equal 'Title',   result.title
-      assert_equal 'Content', result.content
+      assert_equal quote.id,      result.id
+      assert_equal quote.author,  result.author
+      assert_equal quote.title,   result.title
+      assert_equal quote.content, result.content
     end
   end
 
