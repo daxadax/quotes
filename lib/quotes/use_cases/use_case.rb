45b41a18e595225468507da600414f7c0890a114
Dax <d.dax@email.com>
2014-07-15 10:01:33 +0200

    Add quote_boundary to use_case base class

--- lib/quotes/use_cases/use_case.rb
+++ lib/quotes/use_cases/use_case.rb
@@ -7,6 +7,10 @@ module Quotes
         @gateway ||= Gateways::QuotesGateway.new
       end
 
+      def quote_boundary
+        @boundary ||= Services::QuoteBoundary.new
+      end
+
     end
   end
 end
