7525a2f8b5c00a3286071e7508202272eae156f2
Dax <d.dax@email.com>
2014-07-24 18:26:45 +0200

    Add get_quotes use case

--- lib/quotes/use_cases.rb
+++ lib/quotes/use_cases.rb
@@ -1,6 +1,7 @@
 require 'quotes/use_cases/use_case'
 require 'quotes/use_cases/create_quote'
 require 'quotes/use_cases/get_quote'
+require 'quotes/use_cases/get_quotes'
 require 'quotes/use_cases/update_quote'
 require 'quotes/use_cases/delete_quote'
 require 'quotes/use_cases/update_links'
