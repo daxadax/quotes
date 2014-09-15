5faedc997ef62b7ec406b59c40b96be3e94410d8
Dax <d.dax@email.com>
2014-09-15 23:28:20 +0200

    Get all gateway specs working again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,70 @@ Dir.glob('./spec/support/*.rb')  { |f| require f }
 require 'quotes'
 
 ENV['test'] = '1'
-ENV['DATABASE_URL'] = 'sqlite://quotes-test.db'
+
+
+class FakeGatewayBackend
+
+  def initialize
+    @memories = Hash.new
+    @next_id = 0
+  end
+
+  def reset
+    @memories.clear
+    @next_id = 0
+  end
+
+  def insert(quote)
+    quote[:id] = next_id
+
+    @memories[quote[:id]] = quote
+    quote[:id]
+  end
+
+  def get(id)
+    @memories[id]
+  end
+
+  def all
+    @memories.values
+  end
+
+  def update(quote)
+    @memories[quote[:id]] = quote
+    quote[:id]
+  end
+
+  def delete(id)
+    @memories.delete(id)
+  end
+
+  def toggle_star(id)
+    quote = get(id)
+    quote[:starred] == true ? quote[:starred] = false : quote[:starred] = true
+
+    update quote
+  end
+
+  private
+
+  def next_id
+    @next_id += 1
+  end
+
+end
 
 class Minitest::Spec
   include Support::AssertionHelpers
   include Support::FactoryHelpers
   include Quotes
+
+  @@gateway_backend_stub = FakeGatewayBackend.new
+
+  Quotes::Gateways::Gateway.send(:define_method, :backend_for_quotes) do
+    @@gateway_backend_stub
+  end
+
+  after { @@gateway_backend_stub.reset }
 
 end
