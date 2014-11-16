9d954b7996132f039932883471675d66f60c6011
Dax <d.dax@email.com>
2014-11-16 17:41:06 +0100

    Add get publication use case

---
+++ lib/quotes/use_cases/get_publication.rb
@@ -0,0 +1,30 @@
+require 'quotes/services/publication_boundary'
+
+module Quotes
+  module UseCases
+    class GetPublication < UseCase
+
+      Publication = Services::PublicationBoundary::Publication
+      Result = Bound.required( :publication => Publication )
+
+      def initialize(input)
+        ensure_valid_input!(input[:uid])
+
+        @uid = input[:uid]
+      end
+
+      def call
+        Result.new(:publication => get_publication)
+      end
+
+      private
+
+      def get_publication
+        publication = publications_gateway.get(@uid)
+
+        publication_boundary.for publication
+      end
+
+    end
+  end
+end
