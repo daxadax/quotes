d8d6427e097f49ae726a4195bad43149cd953c13
Dax <d.dax@email.com>
2015-01-13 21:46:11 +0100

    Update kindle_importer

--- lib/quotes/services/kindle_importer.rb
+++ lib/quotes/services/kindle_importer.rb
@@ -4,14 +4,19 @@ module Quotes
   module Services
     class KindleImporter < Service
 
-      def initialize
-        @parser = KindleClippings::Parser.new
-      end
-
-      def import(input)
+      def initialize(user_uid, input, autotagger = nil)
         validate(input)
 
-        convert_to_excerpts(input)
+        @user_uid = user_uid
+        @input = input
+        @autotagger = autotagger || Services::Autotag
+      end
+
+      def import
+        clippings.map do |clipping|
+          next if clipping.type == :Bookmark
+          build_and_tag clipping
+        end.compact
       end
 
       private
@@ -22,27 +27,60 @@ module Quotes
         raise_argument_error(reason, input) if input.nil? || input.empty?
       end
 
-      def convert_to_excerpts(input)
-        clippings = parse input
+      def build_and_tag(clipping)
+        quote = build_quote_from clipping
 
-        clippings.map do |clipping|
-          build_excerpt_from clipping unless clipping.type == :Bookmark
-        end.compact
+        return if quote.nil?
+        autotag quote
       end
 
-      def build_excerpt_from(clipping)
-        author  = clipping.author
-        title   = clipping.book_title
-        content = clipping.content
-        page    = clipping.page
+      def autotag(quote)
+        autotagger.new(quote).run
+      end
 
-        unless content.empty?
-          Entities::Quote.new(author, title, content, {:page_number => page})
+      def build_quote_from(clipping)
+        return if clipping.content.empty?
+
+        publication = find_or_create_publication(clipping.author, clipping.book_title)
+        content = clipping.content
+        options = { :page_number => clipping.page }
+
+        Entities::Quote.new @user_uid, content, publication, options
+      end
+
+      def find_or_create_publication(author, title)
+        existing_publication = publication_gateway.all.detect do |publication|
+          publication.author == author && publication.title == title
+        end
+
+        if existing_publication
+          return existing_publication
+        else
+          create_new_publication(author, title)
         end
       end
 
+      def create_new_publication(author, title)
+        p = Entities::Publication.new(@user_uid, author, title, 'kindle import', 0)
+        uid = publication_gateway.add p
+        publication_gateway.get uid
+      end
+
+      def clippings
+        parse @input
+      end
+
       def parse(file_content)
-        @parser.parse file_content
+        parser = KindleClippings::Parser.new
+        parser.parse file_content
+      end
+
+      def autotagger
+        @autotagger
+      end
+
+      def publication_gateway
+        @publication_gateway ||= Gateways::PublicationsGateway.new
       end
 
     end
--- spec/services/kindle_importer_spec.rb
+++ spec/services/kindle_importer_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 class KindleImporterSpec < ServiceSpec
-  let(:kindle_importer) { Services::KindleImporter.new }
-  let(:input) do
-    File.read("spec/support/sample_kindle_clippings.txt")
+  let(:user_uid) { 23 }
+  let(:autotagger) { FakeAutotagService }
+  let(:input) { File.read("spec/support/sample_kindle_clippings.txt") }
+  let(:kindle_importer) do
+    Services::KindleImporter.new(user_uid, input, autotagger)
   end
 
-  let(:result) { kindle_importer.import(input) }
-
-  before {skip}
+  let(:result) { kindle_importer.import }
 
   describe 'import' do
 
@@ -23,8 +23,11 @@ class KindleImporterSpec < ServiceSpec
     describe 'with valid input' do
       let(:first_result)  { result[0] }
 
-      it 'parses the file into exceprts' do
+      it 'parses the file into quotes and autotags them' do
         assert_kind_of Entities::Quote, first_result
+        assert_equal 'Ernest Becker', first_result.author
+        assert_equal 'The Denial of Death', first_result.title
+        assert_includes first_result.tags, 'autotagged'
       end
 
       it 'returns an array of objects' do
@@ -34,6 +37,43 @@ class KindleImporterSpec < ServiceSpec
       it 'only parses notes and highlights' do
         assert_equal 2, result.size
       end
+
+      describe 'used publications' do
+        describe 'with existing publications' do
+          before do
+            create_publication :author => 'Ernest Becker',
+              :title => 'The Denial of Death',
+              :publisher => 'Previously added publisher'
+          end
+
+          it 'uses existing publications if author and title match' do
+            assert_equal 'Ernest Becker', result.first.author
+            assert_equal 'The Denial of Death', result.first.title
+            assert_equal 'Previously added publisher', result.first.publisher
+          end
+        end
+
+        describe 'with no existing publications' do
+          it 'creates a new publication' do
+            assert_equal 'Ernest Becker', result.first.author
+            assert_equal 'The Denial of Death', result.first.title
+            assert_equal 'kindle import', result.first.publisher
+          end
+        end
+      end
+    end
+
+  end
+
+  class FakeAutotagService
+
+    def initialize(quote)
+      @quote = quote
+    end
+
+    def run
+      @quote.tags = ['autotagged']
+      @quote
     end
 
   end
