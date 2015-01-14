4dae9e51719d830df70a64177226d18e4e7b78a2
Dax <d.dax@email.com>
2015-01-14 10:17:52 +0100

    Add update_tags task

---
+++ lib/quotes/tasks/update_tags.rb
@@ -0,0 +1,28 @@
+module Quotes
+  module Tasks
+    class UpdateTags < Task
+
+        def run
+          quotes.each do |quote|
+            updated = update_tags quote
+            gateway.update updated
+          end
+        end
+
+        private
+
+        def update_tags(quote)
+          Services::Autotag.new(quote).run
+        end
+
+        def quotes
+          gateway.all
+        end
+
+        def gateway
+          @gateway ||= Gateways::QuotesGateway.new
+        end
+
+    end
+  end
+end
