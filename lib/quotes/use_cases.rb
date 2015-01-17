a7a08f3b9d5f291b725024f25e673c7ecdcf9cdc
Dax <d.dax@email.com>
2015-01-17 19:19:24 +0100

    Change how files are required in use_cases

--- lib/quotes/use_cases.rb
+++ lib/quotes/use_cases.rb
@@ -1,5 +1,20 @@
 require 'quotes/use_cases/use_case'
-Dir.glob('./lib/quotes/use_cases/*.rb') { |f| require f }
+require 'quotes/use_cases/create_quote'
+require 'quotes/use_cases/get_quote'
+require 'quotes/use_cases/get_quotes'
+require 'quotes/use_cases/update_quote'
+require 'quotes/use_cases/delete_quote'
+require 'quotes/use_cases/update_links'
+require 'quotes/use_cases/search'
+
+require 'quotes/use_cases/import_quotes'
+
+require 'quotes/use_cases/create_publication'
+require 'quotes/use_cases/get_publication'
+require 'quotes/use_cases/get_publications'
+require 'quotes/use_cases/update_publication'
+require 'quotes/use_cases/delete_publication'
+
 
 module Quotes
   module UseCases
