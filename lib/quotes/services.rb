8451bfbd998b83c7ab3da018ff27bad81994e06d
Dax <d.dax@email.com>
2015-01-18 16:47:21 +0100

    Explicitly require services

--- lib/quotes/services.rb
+++ lib/quotes/services.rb
@@ -1,5 +1,9 @@
 require 'quotes/services/service'
-Dir.glob('./lib/quotes/services/*.rb') { |f| require f }
+require 'quotes/services/quote_boundary'
+require 'quotes/services/publication_boundary'
+require 'quotes/services/links_updater'
+require 'quotes/services/kindle_importer'
+require 'quotes/services/autotag'
 
 module Quotes
   module Services
